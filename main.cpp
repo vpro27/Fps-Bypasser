37e527914d68e658d4440b276ae8522cc5cf49f8
vpro27 <veer.waghela1@gmail.com>
2026-05-30 15:56:00 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,7 @@
-#include <Geode/Geode.hpp>
-#include <Geode/modify/MenuLayer.hpp>
+##include <Geode/Geode.hpp>
 
 using namespace geode::prelude;
 
-class $modify(MenuLayer) {
-    bool init() {
-        if (!MenuLayer::init()) {
-            return false;
-        }
-
-        // Set the game to 120 FPS on launch
-        CCDirector::sharedDirector()->setAnimationInterval(1.0f / 120.0f);
-
-        return true;
-    }
-};
+$execute {
+    log::info("Hello from my FPS mod!");
+}
