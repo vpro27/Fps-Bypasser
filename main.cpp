d0216ba407f1911de66e66b6d4a36af20eb3491f
vpro27 <veer.waghela1@gmail.com>
2026-05-30 15:16:06 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,34 +3,13 @@
 
 using namespace geode::prelude;
 
-class $modify(MyMenuLayer, MenuLayer) {
+class $modify(MenuLayer) {
     bool init() {
         if (!MenuLayer::init()) return false;
 
-        auto mainMenu = this->getChildByID("main-menu");
-        if (mainMenu) {
-            auto buttonSprite = CCSprite::createWithSpriteFrameName("GJ_fxBtn_001.png");
-            if (!buttonSprite) {
-                buttonSprite = CCSprite::createWithSpriteFrameName("GJ_playBtn2_001.png");
-            }
-            
-            auto bypassButton = CCMenuItemSpriteExtra::create(
-                buttonSprite,
-                this,
-                menu_selector(MyMenuLayer::onBypassButtonTap)
-            );
-
-            mainMenu->addChild(bypassButton);
-            bypassButton->setID("mobile-fps-bypass-btn");
-            mainMenu->updateLayout();
-        }
+        // Force the game engine to run at 120 FPS immediately on launch
+        CCDirector::sharedDirector()->setAnimationInterval(1.0f / 120.0f);
 
         return true;
-    }
-
-    void onBypassButtonTap(CCObject* sender) {
-        // Set to 120 FPS instantly when the button is pressed
-        CCDirector::sharedDirector()->setAnimationInterval(1.0f / 120.0f);
-        FLAlertLayer::create("FPS Bypass", "FPS has been set to 120!", "OK")->show();
     }
 };
