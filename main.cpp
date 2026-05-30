5b3b83b54ec25f9b8bb01b3d477c724d5dbf8cdc
vpro27 <veer.waghela1@gmail.com>
2026-05-30 15:46:09 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,9 +5,11 @@ using namespace geode::prelude;
 
 class $modify(MenuLayer) {
     bool init() {
-        if (!MenuLayer::init()) return false;
+        if (!MenuLayer::init()) {
+            return false;
+        }
 
-        // Force the game engine to run at 120 FPS immediately on launch
+        // Set the game to 120 FPS on launch
         CCDirector::sharedDirector()->setAnimationInterval(1.0f / 120.0f);
 
         return true;
